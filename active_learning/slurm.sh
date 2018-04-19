2a55e9f57f0e35f86f795670e492a5c1483326c2
Ishita Ankit <iankit@gypsum.cm.cluster>
2018-04-18 23:16:01 -0400

    edited slurm

--- active_learning/slurm.sh
+++ active_learning/slurm.sh
@@ -11,6 +11,8 @@
 # Log what we're running and where.
 echo $SLURM_JOBID - `hostname` >> ~/slurm-jobs.txt
 
-python process_logits.py -s_dev ../active_data/newsqa_files/combined_logits_newsqa.p -p 1 -s ../active_data/newsqa/train_dump.json  -t ../active_data/newsqa_files/Softmax_contrast_top1.json --score-type 3 
+python process_logits.py -s ../active_data/squad/train-v1.1.json -t ../active_data/newsqa/train_dump.json -t_logits ../active_data/newsqa_files/combined_logits_newsqa.p  --score-type 3 -t_dump ../active_data/softmax_contrast_1.json -p 1
+
+#python process_logits.py -s_dev ../active_data/newsqa_files/combined_logits_newsqa.p -p 1 -s ../active_data/newsqa/train_dump.json  -t ../active_data/newsqa_files/Softmax_contrast_top1.json --score-type 3 
 #python process_logits.py -s ../active_data/newsqa/train_dump.json -feat_label_p ../active_data/newsqa_files/classifier_input.p  -t ../active_data/newsqa_files/classifier_top10.json
 #python process_logits.py  -c_i ../active_data/newsqa_files/context_id_combine.p  -logits ../active_data/newsqa_files/combined_logits_newsqa.p  --other_features_file ../active_data/newsqa_files/newsqa_merged.csv -feat_label_p ../active_data/newsqa_filesclassifier_input.p
